b0072999ec12b9e68f6c94df561148a7afaf4d44
Jose Angel Hernandez Radilla <jhernandezr1313@alumno.ipn.mx>
2021-09-22 13:50:22 -0500

    __dirname,__filename,process.cwd

---
+++ 02-variables-globales/basicos.js
@@ -0,0 +1,9 @@
+/*
+__dirname: Obtiene el nombre completo del directorio donde se encuentra el archivo actualmente ejecutado
+__filename: obtenga el nombre del archivo con la ruta absoluta completa del archivo actualmente ejecutado
+process.cwd (): Obtiene el nombre del directorio de la carpeta cuando el comando de nodo se está ejecutando actualmente
+*/
+
+console.log(__dirname)
+console.log(__filename)
+console.log(process.cwd())
